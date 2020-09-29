407e9459d992ff58a2420db36f1f494a17186c2a
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-09-28 22:22:20 -0300

    adds style on navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,13 +1,18 @@
 <template>
-  <nav>
-    <a>BitClass</a>
-    <div>
-      <a>UCL</a>
-      <a>VIDEOS</a>
+  <nav class="flex flex-row justify-between items-center w-8/12 h-32">
+    <a class="font-fredoka text-redbits text-5xl">BitClass</a>
+    <div class="flex flex-row justify-between w-40">
+      <a class="nav-btn">UCL</a>
+      <a class="nav-btn text-redbits">Videos</a>
     </div>
   </nav>
 </template>
 
-<script lang="ts">
-export default {}
-</script>
+<style scoped>
+a {
+  @apply cursor-pointer;
+}
+.nav-btn {
+  @apply w-auto text-2xl text-center;
+}
+</style>
