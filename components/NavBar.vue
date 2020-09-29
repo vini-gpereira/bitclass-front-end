288fb58439f38f2335301fe988ac63e162aaf912
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-09-28 22:42:32 -0300

    adds hrefs and uses nuxtLinks

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,17 +1,16 @@
 <template>
   <nav class="flex flex-row justify-between items-center w-8/12 h-32">
-    <a class="font-fredoka text-redbits text-5xl">BitClass</a>
+    <NuxtLink to="/" class="font-fredoka text-redbits text-5xl"
+      >BitClass</NuxtLink
+    >
     <div class="flex flex-row justify-between w-40">
-      <a class="nav-btn">UCL</a>
-      <a class="nav-btn text-redbits">Videos</a>
+      <a href="https://codelab.ime.usp.br/#/" class="nav-btn">UCL</a>
+      <NuxtLink to="/videos" class="nav-btn text-redbits">Videos</NuxtLink>
     </div>
   </nav>
 </template>
 
 <style scoped>
-a {
-  @apply cursor-pointer;
-}
 .nav-btn {
   @apply w-auto text-2xl text-center;
 }
