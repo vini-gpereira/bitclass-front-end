7264430e16969549c8a65efda4674ce1a8ef8d9b
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-09-28 21:56:10 -0300

    finishes tailwindcss configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,32 @@ export default {
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
+  tailwindcss: {
+    config: {
+      future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+      },
+      theme: {
+        extend: {
+          colors: {
+            redbits: '#ff4b5c',
+            graybits: {
+              100: '#454545',
+              500: '#3a3a3a',
+              900: '#333333',
+            },
+            whitebits: '#f5f5f5',
+          },
+          fontFamily: {
+            fredoka: ['Fredoka One', 'cursive'],
+            mont: ['Montserrat', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+          },
+        },
+      },
+    },
+  },
+
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 }
