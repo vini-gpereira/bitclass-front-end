343da5bd4989e5d39245b778d6d5a7f76d96cbc6
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-10-01 00:49:22 -0300

    starts to add responsiveness on navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,11 +1,22 @@
 <template>
-  <nav class="flex flex-row justify-between items-center w-8/12 h-32">
-    <NuxtLink to="/" class="font-fredoka text-redbits text-5xl"
+  <nav
+    class="flex flex-row justify-between items-center w-10/12 h-24 md:w-8/12 md:h-32"
+  >
+    <NuxtLink to="/" class="font-fredoka text-redbits text-4xl md:text-5xl"
       >BitClass</NuxtLink
     >
-    <div class="flex flex-row justify-between w-40">
-      <a href="https://codelab.ime.usp.br/#/" class="nav-btn">UCL</a>
-      <NuxtLink to="/videos" class="nav-btn text-redbits">Videos</NuxtLink>
+    <div>
+      <a
+        href="https://codelab.ime.usp.br/#/"
+        class="nav-btn mr-2 hidden md:inline-block"
+        >UCL</a
+      >
+      <NuxtLink
+        to="/videos"
+        class="nav-btn text-redbits ml-2 hidden md:inline-block"
+        >Videos</NuxtLink
+      >
+      <fa-icon icon="bars" class="md:hidden"></fa-icon>
     </div>
   </nav>
 </template>
