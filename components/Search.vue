fb8cbe76e26959bd9f05166f9ae396dc8a7b2885
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-10-07 20:47:36 -0300

    feat: creates search component scaffold

---
+++ components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div>
+      <input
+        v-model="searchInput"
+        type="text"
+        placeholder="Procure pelo conteúdo"
+      />
+      <span>
+        <v-icon
+          v-if="searchInput"
+          icon="clear"
+          @click="searchInput = ''"
+        ></v-icon>
+        <div v-if="searchInput"></div>
+        <v-icon icon="search"></v-icon>
+      </span>
+    </div>
+    <div>
+      <button value="videos">Videos</button>
+      <button>Assuntos</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {
+      searchInput: '',
+    }
+  },
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-col items-center text-whitebits">
     <NavBar />
+    <Search />
     <Nuxt />
   </div>
 </template>
