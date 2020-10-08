973f2ebcebcdf532b330e12c72c690e86a2a87a0
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-10-07 23:37:09 -0300

    feat(navbar): style and text changes

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-full">
+  <div class="w-full mt-4">
     <nav
-      class="flex flex-row justify-between items-center mx-auto w-10/12 h-16 md:w-8/12 md:h-32"
+      class="flex flex-row justify-between items-center mx-auto w-10/12 h-12 md:w-8/12 md:h-32"
     >
       <NuxtLink to="/" class="font-fredoka text-redbits text-4xl md:text-5xl"
         >BitClass</NuxtLink
@@ -12,9 +12,7 @@
           class="nav-btn mr-2 hidden md:inline-block"
           >UCL</a
         >
-        <NuxtLink
-          to="/about"
-          class="nav-btn text-redbits ml-2 hidden md:inline-block"
+        <NuxtLink to="/about" class="nav-btn ml-2 hidden md:inline-block"
           >Sobre</NuxtLink
         >
         <button class="w-auto h-auto" @click="openMenu">
@@ -24,14 +22,13 @@
     </nav>
     <ul
       id="menu"
-      class="max-h-0 md:hidden overflow-hidden transition-maxh duration-300 ease-out bg-graybits-900"
+      class="divide-y divide-graybits-100 max-h-0 md:hidden overflow-hidden transition-maxh duration-300 ease-out bg-graybits-900"
     >
       <li>
         <a href="https://codelab.ime.usp.br/#/" class="nav-btn">UCL</a>
       </li>
-      <hr class="w-11/12 mx-auto" />
       <li>
-        <NuxtLink to="/videos" class="nav-btn text-redbits">Videos</NuxtLink>
+        <NuxtLink to="/about" class="nav-btn">Sobre</NuxtLink>
       </li>
     </ul>
   </div>
@@ -67,6 +64,6 @@ ul {
 }
 
 .nav-btn {
-  @apply text-2xl text-center;
+  @apply text-xl text-left w-full h-full leading-12;
 }
 </style>
