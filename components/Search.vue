7dca187240f8a7e0f9f848a2256a2dfb940f4ed7
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-10-07 23:35:52 -0300

    feat: added style and logic to component

--- components/Search.vue
+++ components/Search.vue
@@ -1,24 +1,38 @@
 <template>
-  <section>
-    <div>
+  <section class="mx-auto w-10/12 h-64 text-sm mt-5">
+    <div
+      class="px-3 h-10 flex flex-row bg-whitebits border-2 border-solid rounded-full transition duration-300 border-2 border-solid focus-within:border-redbits"
+    >
       <input
         v-model="searchInput"
-        type="text"
+        type="search"
         placeholder="Procure pelo conteúdo"
+        class="text-black w-full mr-3"
       />
-      <span>
-        <v-icon
+      <div class="text-xs flex flex-row items-center">
+        <fa-icon
           v-if="searchInput"
-          icon="clear"
+          icon="times"
+          class="text-black fa-lg mr-3 cursor-pointer"
           @click="searchInput = ''"
-        ></v-icon>
-        <div v-if="searchInput"></div>
-        <v-icon icon="search"></v-icon>
-      </span>
+        ></fa-icon>
+        <div v-if="searchInput" class="w-px h-8 bg-gray-500 mr-3"></div>
+        <fa-icon
+          icon="search"
+          class="text-redbits fa-lg cursor-pointer"
+        ></fa-icon>
+      </div>
     </div>
-    <div>
-      <button value="videos">Videos</button>
-      <button>Assuntos</button>
+    <div class="flex flex-row justify-between mt-2">
+      <button :class="btnsClasses.videos" @click="handleBtnClick('videos')">
+        Videos
+      </button>
+      <button
+        :class="btnsClasses.categories"
+        @click="handleBtnClick('categories')"
+      >
+        Assuntos
+      </button>
     </div>
   </section>
 </template>
@@ -29,7 +43,31 @@ export default {
   data() {
     return {
       searchInput: '',
+      btnsClasses: {
+        videos: 'selected',
+        categories: 'no-selected',
+      },
+      searchType: 'videos',
     }
+  },
+  methods: {
+    handleBtnClick(searchType) {
+      this.btnsClasses[this.selectedBtn] = 'no-selected'
+      this.btnsClasses[searchType] = 'selected'
+      this.selectedBtn = searchType
+    },
   },
 }
 </script>
+
+<style scoped>
+button {
+  @apply rounded-full h-10 w-al-mid border-solid border border-redbits;
+}
+.selected {
+  @apply bg-redbits;
+}
+.no-selected {
+  @apply bg-transparent text-redbits;
+}
+</style>
