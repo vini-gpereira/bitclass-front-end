084b31fcfb302b28c1b5d367f447b9fc8dce4357
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-10-08 23:29:32 -0300

    refactor: change style and some html tags positions

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,28 +1,22 @@
 <template>
   <div class="w-full mt-4">
     <nav
-      class="flex flex-row justify-between items-center mx-auto w-10/12 h-12 md:w-8/12 md:h-32"
+      class="items-center w-10/12 h-12 mx-auto flex-row-between md:w-8/12 md:h-32"
     >
-      <NuxtLink to="/" class="font-fredoka text-redbits text-4xl md:text-5xl"
+      <NuxtLink to="/" class="text-4xl font-fredoka text-redbits md:text-5xl"
         >BitClass</NuxtLink
       >
-      <div>
-        <a
-          href="https://codelab.ime.usp.br/#/"
-          class="nav-btn mr-2 hidden md:inline-block"
-          >UCL</a
-        >
-        <NuxtLink to="/about" class="nav-btn ml-2 hidden md:inline-block"
-          >Sobre</NuxtLink
-        >
-        <button class="w-auto h-auto" @click="openMenu">
-          <fa-icon icon="bars" class="md:hidden fa-lg"></fa-icon>
-        </button>
+      <div class="hidden text-2xl md:flex-row-between">
+        <a href="https://codelab.ime.usp.br/#/" class="mr-2 nav-btn">UCL</a>
+        <NuxtLink to="/about" class="ml-2 nav-btn">Sobre</NuxtLink>
       </div>
+      <button class="inline-block w-auto h-auto md:hidden" @click="openMenu">
+        <fa-icon icon="bars" class="fa-lg"></fa-icon>
+      </button>
     </nav>
     <ul
       id="menu"
-      class="divide-y divide-graybits-100 max-h-0 md:hidden overflow-hidden transition-maxh duration-300 ease-out bg-graybits-900"
+      class="overflow-hidden duration-300 ease-out divide-y divide-graybits-100 max-h-0 md:hidden transition-maxh bg-graybits-900"
     >
       <li>
         <a href="https://codelab.ime.usp.br/#/" class="nav-btn">UCL</a>
@@ -59,11 +53,10 @@ export default {
 <style lang="scss" scoped>
 ul {
   li {
-    @apply w-full px-4 h-12 flex items-center;
+    @apply w-full px-4 h-12 flex items-center text-xl;
   }
 }
-
 .nav-btn {
-  @apply text-xl text-left w-full h-full leading-12;
+  @apply text-left w-full h-full leading-12 cursor-pointer;
 }
 </style>
