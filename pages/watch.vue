c9335f74be37967256e4e046f6e64536d7e95310
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-11-02 22:39:13 -0300

    feat: create watch route

---
+++ pages/watch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <section>
+        <h1>Titulo do video</h1>
+        <fa-icon icon="bars"></fa-icon>
+      </section>
+      <Video :video-id="videoId" />
+      <Description :video-id="videoId" />
+    </div>
+    <div>
+      <Suggestions :video-id="videoId" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      videoId: '',
+    }
+  },
+  mounted() {
+    this.videoId = this.$route.query.v
+
+    if (!this.videoId) this.$router.push({ name: 'index' })
+  },
+}
+</script>
