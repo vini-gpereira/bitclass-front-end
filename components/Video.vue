b6ce991f45b351a7e39ca28d15e8d5fc326d96cf
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-11-02 22:37:55 -0300

    chore: create initial video component

---
+++ components/Video.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <iframe :src="`https://www.youtube.com.br/embed/${videoId}`"></iframe>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    videoId: {
+      type: String,
+      default: null,
+    },
+  },
+}
+</script>
