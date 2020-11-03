c98f4391aeced927ba3efa14122f6ec4f475d54a
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-11-02 22:38:38 -0300

    chore: create initial suggestions component

---
+++ components/Suggestions.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="(video, index) in suggestions" :key="index">
+      <VideoResult :video="video" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import { getSuggestions } from '@/mock/api'
+
+export default {
+  props: {
+    videoId: {
+      type: String,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      suggestions: null,
+    }
+  },
+  mounted() {
+    this.suggestions = getSuggestions(this.videoId)
+  },
+}
+</script>
