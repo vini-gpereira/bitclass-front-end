0faae959834acb61f2ccbdee184c14e4c2413afa
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-11-08 19:14:29 -0300

    chore: add scroll on tab item overflow

--- components/Description.vue
+++ components/Description.vue
@@ -26,7 +26,12 @@
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
               reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
+              nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur.
             </p>
           </div>
         </v-tab-item>
@@ -76,6 +81,7 @@ export default {
     @apply flex-grow;
 
     .tab-item-wrapper {
+      max-height: 21rem;
       @apply p-3 bg-graybits-500 overflow-scroll;
     }
   }
