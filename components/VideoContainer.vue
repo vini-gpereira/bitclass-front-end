2ce3cfe5a9b8faf04c324290a55f8ea7359cfc06
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-11-07 21:07:56 -0300

    chore: create VideoContainer component

---
+++ components/VideoContainer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <section class="flex flex-row justify-between px-3 mb-2 text-xl">
+      <h1>Titulo do video</h1>
+      <button class="hidden">
+        <fa-icon icon="bars"></fa-icon>
+      </button>
+    </section>
+    <Video :video-id="videoId" />
+    <section
+      class="flex flex-row items-center px-3 pb-2 mt-2 border-b-2 border-graybits-900"
+    >
+      <div class="flex flex-col">
+        <span class="text-lg">Vinicius Pereira</span>
+        <span class="text-xs">02/11/2020</span>
+      </div>
+      <v-btn icon color="var(--white)">
+        <fa-icon icon="angle-down" class="fa-lg"></fa-icon>
+      </v-btn>
+    </section>
+    <Description :video-id="videoId" :show-description="true" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    videoId: {
+      type: String,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style></style>
