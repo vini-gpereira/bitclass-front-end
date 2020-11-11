fe95ba712637f0374cf96eb3fb351b5870d44bbc
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-11-10 23:48:09 -0300

    chore: add style to suggestion item component

--- components/SuggestionItem.vue
+++ components/SuggestionItem.vue
@@ -1,13 +1,18 @@
 <template>
-  <NuxtLink :to="{ path: 'watch', query: { v: video.id } }">
-    <div>
-      <img :src="`https://img.youtube.com/vi/${video.id}/mqdefault.jpg`" />
+  <NuxtLink
+    :to="{ path: 'watch', query: { v: video.id } }"
+    class="video-link-container"
+  >
+    <div class="image-wrapper">
+      <img
+        :src="`https://img.youtube.com/vi/${video.id}/mqdefault.jpg`"
+        alt="suggestion-thumbnail"
+      />
     </div>
-    <div>
+    <div class="suggestion-data-container">
       <h1>{{ video.title }}</h1>
-      <span>
-        {{ video.author }} • {{ video.duration }} • {{ video.postDate }}
-      </span>
+      <span>{{ video.author }}</span>
+      <span>{{ video.duration }} • {{ video.postDate }}</span>
     </div>
   </NuxtLink>
 </template>
@@ -22,3 +27,29 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.video-link-container {
+  @apply flex flex-col;
+
+  .image-wrapper {
+    @apply w-full flex flex-col border border-whitebits;
+
+    img {
+      @apply w-full;
+    }
+  }
+
+  .suggestion-data-container {
+    @apply flex flex-col;
+
+    h1 {
+      @apply text-xl mt-1;
+    }
+
+    span {
+      @apply text-base;
+    }
+  }
+}
+</style>
