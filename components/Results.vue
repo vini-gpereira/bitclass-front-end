3ca0f1654dfc189b7da464640a18c567a15129f4
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-12-03 23:54:03 -0300

    chore: rewrite Results component using vuetify tabs

--- components/Results.vue
+++ components/Results.vue
@@ -1,33 +1,101 @@
 <template>
-  <div class="w-full mt-8 mb-16 md:w-10/12 md:mt-16">
-    <ul v-if="type === 'videos'" class="flex flex-col items-center">
-      <li
-        v-for="(video, index) in results"
-        :key="index"
-        class="w-full p-2 mb-2 border-b-2 border-graybits-100 md:p-0 md:pb-6 md:mb-6"
-      >
-        <VideoResult :video="video" />
-      </li>
-    </ul>
-    <ul v-else-if="type === 'categories'" class="px-2 md:text-2xl">
-      <li v-for="(category, index) in results" :key="index" class="w-full">
-        <CategoryResult :id="index" :category="category" />
-      </li>
-    </ul>
+  <div class="results-container">
+    <v-tabs
+      v-model="tab"
+      background-color="var(--gray)"
+      color="var(--white)"
+      class="tabs"
+    >
+      <v-tabs-slider color="var(--red)"></v-tabs-slider>
+      <v-tab active-class="active-tab" class="tab">Videos</v-tab>
+      <v-tab active-class="active-tab" class="tab">Categories</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <ul class="flex flex-col items-center">
+          <li
+            v-for="(video, index) in results['videos']"
+            :key="index"
+            class="w-full p-2 mb-2 border-b-2 border-graybits-100 md:p-0 md:pb-6 md:mb-6"
+          >
+            <VideoResult :video="video" />
+          </li>
+        </ul>
+      </v-tab-item>
+      <v-tab-item>
+        <ul class="px-2 md:text-2xl">
+          <li
+            v-for="(category, index) in results['categories']"
+            :key="index"
+            class="w-full"
+          >
+            <CategoryResult :id="index" :category="category" />
+          </li>
+        </ul>
+      </v-tab-item>
+    </v-tabs-items>
   </div>
 </template>
 
 <script>
+const types = ['videos', 'categories']
+
 export default {
   props: {
-    type: {
-      type: String,
-      default: 'videos',
-    },
     results: {
-      type: Array,
-      default: () => [],
+      type: Object,
+      default: () => {},
     },
+  },
+  data() {
+    return {
+      tab: null,
+    }
+  },
+  watch: {
+    tab(newTab) {
+      const type = types[newTab]
+
+      this.$store.commit('changeSearchText', type)
+
+      this.$router.push({
+        query: { ...this.$route.query, type },
+      })
+    },
+  },
+  mounted() {
+    const type = this.$route.query.type || this.$store.state.searchType
+
+    this.$store.commit('changeSearchText', type)
+
+    this.tab = types.indexOf(type)
   },
 }
 </script>
+
+<style lang="scss">
+.results-container {
+  width: 90%;
+  @apply flex flex-col items-center justify-center mb-16;
+
+  .tabs {
+    @apply w-auto;
+
+    .tab {
+      color: var(--white) !important;
+      @apply normal-case text-lg;
+    }
+
+    .active-tab {
+      color: var(--red) !important;
+    }
+  }
+}
+
+@screen md {
+  .results-container {
+    width: 95%;
+    @apply mt-16;
+  }
+}
+</style>
