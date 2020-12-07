64a3897e0219c9d8ff29878ec800e6fcb4f586c1
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-12-07 12:14:22 -0300

    chore: create about page file

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about-container">
+    <h1>Pagina ainda nao existe...</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.about-container {
+  @apply flex items-center justify-center h-full;
+
+  h1 {
+    @apply text-4xl text-graybits-100 font-mont text-center;
+  }
+}
+</style>
